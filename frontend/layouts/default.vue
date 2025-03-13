1d932f040c13e3d132f94b08f3c0086cc7fb279a
Lorenzo <lorenzo.galassi91@gmail.com>
2025-03-13 15:38:09 +0000

    fix: footer spacing

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="flex flex-col w-full">
     <!-- Header -->
-    <div class="flex flex-col " >
+    <div class="flex flex-col items-center md:items-start" >
     <!-- <div class="sticky top-0 flex flex-col w-full bg-white bg-opacity-50" > -->
-      <img :src="`${url}images/Logo_Ascolto_Attivo_nera.png`" alt="Un Parco In Biblioteca - Logo" width="215" height="70" class="my-6 ml-16" />
+      <img :src="`${url}images/Logo_Ascolto_Attivo_nera.png`" alt="Un Parco In Biblioteca - Logo" width="280" height="90" class="md:ml-16" />
       <Divider />
     </div>
     <slot />
 
     <!-- Footer -->
-    <div class="bg-[#1d1d1b] flex flex-row gap-16 py-16 pl-32">
-      <NuxtImg :src="`${url}images/Logo_Ascolto_Attivo_bianco.png`" alt="Un Parco In Biblioteca - Logo" />
+    <div class="bg-[#1d1d1b] flex flex-col md:flex-row md:gap-16 gap-8 py-8 items-center  md:pl-32">
+      <NuxtImg :src="`${url}images/Logo_Ascolto_Attivo_bianco.png`" alt="Un Parco In Biblioteca - Logo" width="280" />
       <div class="flex flex-col text-white">
         <div class="font-black">{{ data.footer.title }}</div>
         <div>{{ data.footer.address }}</div>
