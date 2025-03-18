baf26fe156add996f3212591287183daab473767
Lorenzo <lorenzo.galassi91@gmail.com>
2025-03-18 08:55:35 +0000

    fix: add site title

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,6 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
+  app: {
+      head: {
+        title: 'Un Parco in Biblioteca',
+        htmlAttrs: {
+          lang: 'it',
+        },
+      }
+  },
   runtimeConfig: {
     public: {
       environment: process.env.ENVIRONMENT || 'development',
