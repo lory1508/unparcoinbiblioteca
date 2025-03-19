49129cf9c03b340604beeb8567a4981c5fbc6ef9
Lorenzo <lorenzo.galassi91@gmail.com>
2025-03-19 15:59:24 +0000

    feat: removed time for 14.6 + add Rivolo logo in footer
    update copy in section "Perché un percorso partecipativo?"

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -9,12 +9,20 @@
     <slot />
 
     <!-- Footer -->
-    <div class="bg-[#1d1d1b] flex flex-col lg:flex-row lg:gap-16 gap-8 py-8 items-center  lg:pl-32">
-      <img :src="data.footer.logo" alt="Un Parco In Biblioteca - Logo" width="280" />
-      <div class="flex flex-col text-white">
-        <div class="font-black">{{ data.footer.title }}</div>
-        <div>{{ data.footer.address }}</div>
-        <a :href="`mailto:${data.footer.email}`">{{ data.footer.email }}</a>
+    <div class="bg-[#1d1d1b] w-full justify-center items-center flex">
+      <div class="flex flex-col items-center justify-between gap-16 py-8 lg:flex-row lg:gap-16 w-fit lg:px-32">
+        <div class="flex flex-col items-center text-white lg:flex-row">
+          <img :src="data.footer.logo" alt="Un Parco In Biblioteca - Logo" width="280" />
+          <div class="flex flex-col text-white">
+            <div class="font-black">{{ data.footer.title }}</div>
+            <div>{{ data.footer.address }}</div>
+            <a :href="`mailto:${data.footer.email}`">{{ data.footer.email }}</a>
+          </div>
+        </div>
+
+        <a :href="data.footer.rivoli.href" target="_blank">
+          <img :src="data.footer.rivoli.logo" alt="Logo Città di Rivoli" width="180">
+        </a>
       </div>
     </div>
   </div>
