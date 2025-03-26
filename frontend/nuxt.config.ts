697bd757fbf2a48c268fea0b446feb7d967f88f4
Lorenzo <lorenzo.galassi91@gmail.com>
2025-03-26 13:03:29 +0000

    init decap CMS

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,17 +3,17 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   app: {
-      head: {
-        title: 'Un Parco in Biblioteca',
-        htmlAttrs: {
-          lang: 'it',
-        },
-      }
+    head: {
+      title: 'Un Parco in Biblioteca',
+      htmlAttrs: {
+        lang: 'it',
+      },
+    },
   },
   runtimeConfig: {
     public: {
       environment: process.env.ENVIRONMENT || 'development',
-    }
+    },
   },
 
   nitro: {
@@ -26,20 +26,12 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  modules: ['@nuxt/image', '@nuxt/fonts', '@nuxt/icon'],
+  modules: ['@nuxt/image', '@nuxt/fonts', '@nuxt/icon', '@nuxt/content'],
   fonts: {
     openDyslexic: {
       weights: [400, 600],
       styles: ['normal', 'italic'],
-      subsets: [
-        'cyrillic-ext',
-        'cyrillic',
-        'greek-ext',
-        'greek',
-        'vietnamese',
-        'latin-ext',
-        'latin',
-      ]
+      subsets: ['cyrillic-ext', 'cyrillic', 'greek-ext', 'greek', 'vietnamese', 'latin-ext', 'latin'],
     },
-  }
+  },
 })
