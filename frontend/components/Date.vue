a8527e2f0db3e086b60d920c46fa29a5afd7ac4b
Lorenzo <lorenzo.galassi91@gmail.com>
2025-04-03 16:30:12 +0100

    feat: new dates

--- frontend/components/Date.vue
+++ frontend/components/Date.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex flex-row items-center gap-2">
-    <div 
+    <div
       class="flex items-center -ml-1 h-fit 2xl:h-96"
       :class="{
         '2xl:flex-col': up,
         '2xl:flex-col-reverse': !up,
       }"
     >
-      <div 
+      <div
         class="flex flex-col gap-2 text-center"
         :class="{
           'text-biblioteca-blue': color === 'blue',
@@ -19,11 +19,11 @@
       >
         <div class="text-4xl font-bold custom-title-font">{{ date }}</div>
         <div class="flex flex-col">
-          <div class="text-lg font-black text-nowrap">{{ type }}</div>
-          <div class="text-nowrap">{{ time }}</div>
+          <div class="font-black text-md text-nowrap">{{ type }}</div>
+          <div class="text-md text-nowrap">{{ time }}</div>
         </div>
       </div>
-      <div 
+      <div
         class="hidden w-full h-[88px] border-r-4 2xl:block"
         :class="{
           'border-biblioteca-blue': color === 'blue',
@@ -31,18 +31,16 @@
           'border-biblioteca-red': color === 'red',
           'border-biblioteca-yellow': color === 'yellow',
           'border-biblioteca-purple': color === 'purple',
-          'border-b-4 rounded-br-lg' : up,
-          'border-t-4 rounded-tr-lg' : !up,
+          'border-b-4 rounded-br-lg': up,
+          'border-t-4 rounded-tr-lg': !up,
         }"
       />
-      <div
-        class="2xl:hidden w-full h-[88px] border-b-4"
-      />
+      <div class="2xl:hidden w-full h-[88px] border-b-4" />
     </div>
     <div class="2xl:hidden">
-      <Icon 
-        name="gravity-ui:arrow-right" 
-        size="48" 
+      <Icon
+        name="gravity-ui:arrow-right"
+        size="48"
         :class="{
           'text-biblioteca-blue': color === 'blue',
           'text-biblioteca-green': color === 'green',
@@ -56,17 +54,13 @@
 </template>
 
 <script setup>
-
-const props = defineProps({
-  time: String,
-  date: String,
-  type: String,
-  color: String,
-  up: Boolean,
-})
-
+  const props = defineProps({
+    time: String,
+    date: String,
+    type: String,
+    color: String,
+    up: Boolean,
+  })
 </script>
 
-<style>
-
-</style>
+<style></style>
