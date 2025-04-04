124c12d11bc67b9b46bd563745cd1e91186cad45
Lorenzo <lorenzo.galassi91@gmail.com>
2025-04-04 11:04:54 +0100

    feat: opzione 2

--- frontend/components/Date.vue
+++ frontend/components/Date.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-row items-center justify-center gap-2 h-fit">
     <div class="flex items-center justify-center h-fit">
       <div
-        class="flex flex-col gap-2 px-8 pl-4 text-center border-8 border-r-0 rounded-r-md"
+        class="flex flex-col gap-2 pl-8 pr-16 text-center border-4 rounded-r-full"
         :class="{
           'border-biblioteca-blue text-biblioteca-blue': color === 'blue',
           'border-biblioteca-green text-biblioteca-green': color === 'green',
@@ -17,18 +17,6 @@
           <div class="text-md text-nowrap">{{ time }}</div>
         </div>
       </div>
-      <Icon
-        icon="teenyicons:right-outline"
-        width="128"
-        class="p-0 -ml-11"
-        :class="{
-          'text-biblioteca-blue': color === 'blue',
-          'text-biblioteca-green': color === 'green',
-          'text-biblioteca-red': color === 'red',
-          'text-biblioteca-yellow': color === 'yellow',
-          'text-biblioteca-purple': color === 'purple',
-        }"
-      />
       <!-- <div class="inline-block w-20 overflow-hidden">
         <div
           class="origin-top-left transform rotate-45 border-2 h-[108px]"
