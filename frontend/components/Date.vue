711a7f4a9303fd36a4859a342c5b7b78d9591413
Lorenzo <lorenzo.galassi91@gmail.com>
2025-04-04 10:17:43 +0100

    feat: new timeline

--- frontend/components/Date.vue
+++ frontend/components/Date.vue
@@ -1,20 +1,14 @@
 <template>
-  <div class="flex flex-row items-center gap-2">
-    <div
-      class="flex items-center -ml-1 h-fit 2xl:h-96"
-      :class="{
-        '2xl:flex-col': up,
-        '2xl:flex-col-reverse': !up,
-      }"
-    >
+  <div class="flex flex-row items-center justify-center gap-2 h-fit">
+    <div class="flex items-center justify-center lg:-ml-28 h-fit">
       <div
-        class="flex flex-col gap-2 text-center"
+        class="flex flex-col gap-2 pl-4 pr-8 text-center text-white lg:pl-20"
         :class="{
-          'text-biblioteca-blue': color === 'blue',
-          'text-biblioteca-green': color === 'green',
-          'text-biblioteca-red': color === 'red',
-          'text-biblioteca-yellow': color === 'yellow',
-          'text-biblioteca-purple': color === 'purple',
+          'bg-biblioteca-blue': color === 'blue',
+          'bg-biblioteca-green': color === 'green',
+          'bg-biblioteca-red': color === 'red',
+          'bg-biblioteca-yellow': color === 'yellow',
+          'bg-biblioteca-purple': color === 'purple',
         }"
       >
         <div class="text-4xl font-bold custom-title-font">{{ date }}</div>
@@ -23,32 +17,18 @@
           <div class="text-md text-nowrap">{{ time }}</div>
         </div>
       </div>
-      <div
-        class="hidden w-full h-[88px] border-r-4 2xl:block"
-        :class="{
-          'border-biblioteca-blue': color === 'blue',
-          'border-biblioteca-green': color === 'green',
-          'border-biblioteca-red': color === 'red',
-          'border-biblioteca-yellow': color === 'yellow',
-          'border-biblioteca-purple': color === 'purple',
-          'border-b-4 rounded-br-lg': up,
-          'border-t-4 rounded-tr-lg': !up,
-        }"
-      />
-      <div class="2xl:hidden w-full h-[88px] border-b-4" />
-    </div>
-    <div class="2xl:hidden">
-      <Icon
-        name="gravity-ui:arrow-right"
-        size="48"
-        :class="{
-          'text-biblioteca-blue': color === 'blue',
-          'text-biblioteca-green': color === 'green',
-          'text-biblioteca-red': color === 'red',
-          'text-biblioteca-yellow': color === 'yellow',
-          'text-biblioteca-purple': color === 'purple',
-        }"
-      />
+      <div class="inline-block w-20 overflow-hidden">
+        <div
+          class="origin-top-left transform rotate-45 h-[108px]"
+          :class="{
+            'bg-biblioteca-blue': color === 'blue',
+            'bg-biblioteca-green': color === 'green',
+            'bg-biblioteca-red': color === 'red',
+            'bg-biblioteca-yellow': color === 'yellow',
+            'bg-biblioteca-purple': color === 'purple',
+          }"
+        ></div>
+      </div>
     </div>
   </div>
 </template>
