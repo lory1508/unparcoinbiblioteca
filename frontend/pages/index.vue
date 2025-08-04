c313ba1d3347567ab9f08cf41442b25f839944d6
Lorenzo <lorenzo.galassi91@gmail.com>
2025-08-04 09:30:07 +0200

    feat: fix width

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -137,7 +137,7 @@
     </a>
     <a :href="data.externalLinks.updates.href" target="_blank">
       <div
-        class="flex items-center justify-center h-40 text-xl text-center text-white uppercase transition-all duration-300 scale-110 rounded-full w-96 size-fit custom-title-font hover:scale-125"
+        class="flex items-center justify-center h-40 text-xl text-center text-white uppercase transition-all duration-300 scale-110 rounded-full w-[420px] size-fit custom-title-font hover:scale-125"
         :style="`background-position: center; background-size: contain; background-repeat: no-repeat; background-image: url('${url}${data.externalLinks.updates.img}');`"
       >
         <div class="w-64">
