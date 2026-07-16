aefb68b1dc27998d13a82cb8fc329b09496aa9db
Lorenzo Galassi <lorenzo.galassi91@gmail.com>
2026-07-16 12:10:37 +0200

    fix: add / in img path

--- frontend/pages/cosa-e-stato-fatto.vue
+++ frontend/pages/cosa-e-stato-fatto.vue
@@ -59,7 +59,7 @@
     <a :href="data.externalLinks.updates.href" target="_blank">
       <div
         class="flex items-center justify-center h-40 text-xl text-center text-white uppercase transition-all duration-300 scale-110 rounded-full w-[420px] size-fit custom-title-font hover:scale-125"
-        :style="`background-position: center; background-size: contain; background-repeat: no-repeat; background-image: url('${url}${data.externalLinks.updates.img}');`"
+        :style="`background-position: center; background-size: contain; background-repeat: no-repeat; background-image: url('${url}/${data.externalLinks.updates.img}');`"
       >
         <div class="w-64">
           {{ data.externalLinks.updates.text }}
