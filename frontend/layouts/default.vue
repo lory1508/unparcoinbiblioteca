c73df48075e05da246c6c8dde993d1637da4a8c3
Lorenzo <lorenzo.galassi91@gmail.com>
2026-07-19 12:35:09 +0200

    feat: new mobile menu WIP

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,45 +2,53 @@
   <div
     class="flex flex-col w-full min-h-screen"
     :class="
-      route?.name === 'patti-di-collaborazione'
-        ? 'bg-[#FBF5E9]'
-        : 'bg-gradient-to-br from-sky-50 to-sky-100'
+      route?.name === 'patti-di-collaborazione' ?
+        'bg-[#FBF5E9]'
+      : 'bg-gradient-to-br from-sky-50 to-sky-100'
     "
   >
     <!-- Mobile menu -->
     <div
-      class="fixed z-50 block border-2 rounded-md cursor-pointer bg-zinc-800 bg-opacity-20 backdrop-blur-sm top-5 left-5 border-zinc-400 md:hidden"
-      @click="toggleMenu"
+      class="fixed bottom-0 z-50 self-center block w-10/12 transition-all duration-500 md:hidden"
+      :class="showMenu ? '-translate-y-0' : 'translate-y-48'"
     >
-      <Icon icon="teenyicons:menu-solid" width="40" class="p-1 text-zinc-400" />
-    </div>
-
-    <div
-      class="fixed z-40 flex-col w-full transition-all duration-500 h-fit md:hidden"
-      :class="showMenu ? 'bottom-0' : '-bottom-full'"
-    >
-      <div class="flex items-center justify-center w-full">
+      <div class="flex justify-end w-full px-4">
         <div
-          class="flex flex-col w-full gap-4 px-4 py-6 pt-6 bg-opacity-50 border-t-2 bg-zinc-200 border-zinc-300 backdrop-blur-sm"
+          class="flex flex-col items-center justify-center cursor-pointer w-fit md:hidden"
+          @click="toggleMenu"
         >
-          <NuxtLink
-            to="/"
-            class="w-full px-4 py-2 text-sm text-center text-white rounded-full bg-biblioteca-blue custom-title-font lg:w-fit"
+          <Icon
+            icon="teenyicons:bookmark-solid"
+            height="72"
+            class="rotate-180 opacity-50 text-zinc-200"
+          />
+          <span class="z-40 -mt-10 text-zinc-200">menu</span>
+        </div>
+      </div>
+      <div class="flex-col w-full h-fit md:hidden">
+        <div class="flex items-center justify-center w-full">
+          <div
+            class="flex flex-col w-full gap-4 px-4 py-6 pt-6 bg-opacity-50 border-t-2 rounded-t-xl bg-zinc-200 border-zinc-400 backdrop-blur-sm"
           >
-            Home
-          </NuxtLink>
-          <NuxtLink
-            to="/cosa-e-stato-fatto"
-            class="w-full px-4 py-2 text-sm text-center text-white rounded-full bg-biblioteca-purple custom-title-font lg:w-fit"
-          >
-            Cosa è stato fatto
-          </NuxtLink>
-          <NuxtLink
-            to="/patti-di-collaborazione"
-            class="w-full px-4 py-2 text-sm text-center text-white rounded-full bg-biblioteca-green custom-title-font lg:w-fit"
-          >
-            Patti di collaborazione
-          </NuxtLink>
+            <NuxtLink
+              to="/"
+              class="w-full px-4 py-2 text-sm text-center text-white rounded-full bg-biblioteca-blue custom-title-font lg:w-fit"
+            >
+              Home
+            </NuxtLink>
+            <NuxtLink
+              to="/cosa-e-stato-fatto"
+              class="w-full px-4 py-2 text-sm text-center text-white rounded-full bg-biblioteca-purple custom-title-font lg:w-fit"
+            >
+              Cosa è stato fatto
+            </NuxtLink>
+            <NuxtLink
+              to="/patti-di-collaborazione"
+              class="w-full px-4 py-2 text-sm text-center text-white rounded-full bg-biblioteca-green custom-title-font lg:w-fit"
+            >
+              Patti di collaborazione
+            </NuxtLink>
+          </div>
         </div>
       </div>
     </div>
